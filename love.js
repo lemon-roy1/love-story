9af9c4c94a255957d293c3427d299cdef754a43d
Lemon roy (Nila) <you@example.com>
2023-02-06 12:52:24 -0800

    love-story project file

---
+++ love.js
@@ -0,0 +1,10 @@
+document.getElementById('yes').addEventListener('click',function(){
+    document.getElementById('message').innerText="OF COURSES 💝 " ;
+    to("OK I LOVE YOU 💘")
+})
+
+
+document.getElementById('now').addEventListener('click',function(){
+    document.getElementById('message').innerText="Go Straight 🍼";
+    alert('this is mice you ⛱' )
+})
